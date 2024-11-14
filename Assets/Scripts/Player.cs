d1b60af3554a58dac7b462fc0cf350aa5c70c112
Adam Martin <adam.madram@gmail.com>
2024-11-13 21:06:42 -0600

    Add player script
    Add guest prefab

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[SelectionBase]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int money;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
